19ed49cf2836d5c922afa5dafbafaf476ff8863f
Ygor Simões <contato.ygorsimoes@gmail.com>
2021-02-14 22:30:32 -0300

    Atualização em C#

--- C#/ByteBank/ByteBank.cs
+++ C#/ByteBank/ByteBank.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using ByteBank.Conta;
 using ByteBank.Funcionario;
 
 namespace ByteBank
@@ -12,26 +11,11 @@ namespace ByteBank
             
             // Objetos
             Gerente contaJoao = new Gerente("João Soares Neto", 123456, 7, 500, true);
-            Programador contaCarlos = new Programador("Carlos Magalhães Silveira", 123475, 7, 0, true);
+            Atendente contaCarlos = new Atendente("Carlos Magalhães", 123475, 7, 0, true);
 
             // Escopo
             contaJoao.ExibirExtrato();
             contaCarlos.ExibirExtrato();
-            
-            Console.WriteLine("\n==============================================");
-            
-            contaCarlos.ReceberSalario();
-            contaJoao.ReceberSalario();
-            
-            Console.WriteLine("==============================================\n");
-            
-            contaCarlos.Transferir(10000, contaJoao);
-            
-            Console.WriteLine("==============================================\n");
-            
-            contaJoao.ExibirExtrato();
-            contaCarlos.ExibirExtrato();
-            
         }
     }
 }
